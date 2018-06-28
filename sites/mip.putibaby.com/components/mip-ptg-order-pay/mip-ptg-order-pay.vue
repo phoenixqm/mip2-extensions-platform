e936e63133dacbc54b17a61c666acd680ef6123d
Qi Ming <phoenixqm@gmail.com>
2018-06-28 20:39:27 +0800

    add basic order pay compo

--- sites/mip.putibaby.com/components/mip-ptg-order-pay/mip-ptg-order-pay.vue
+++ sites/mip.putibaby.com/components/mip-ptg-order-pay/mip-ptg-order-pay.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="root">
-
+    <div class='header'>
+      支付订单{{ data.order_number }}，{{ data.text }}
+    </div>
+    <div class='row'>
+      <div class="left">支付金额</div>
+      <div class="right">{{ (data.amount/100).toFixed(2) }} 元</div>
+    </div>
+    <div class="header">
+      支付方式
+    </div>
+    <div class="row" @click="useBalancePay">
+      <div class="left">&nbsp;&nbsp;&nbsp;&nbsp;- 余额支付</div>
+      <div class="right">{{ (data.reward_balance/100).toFixed(2) }} 元</div>
+      <div :calss="{'checked' : balanceChecked , 'unchecked' : !balanceChecked}" />
+    </div>
+    <div class="row" @click="useWxPay">
+      <div class="left">&nbsp;&nbsp;&nbsp;&nbsp;- 微信支付</div>
+      <div class="right">需支付 {{ data.needPay }} 元</div>
+      <div :calss="{'checked' : wxChecked , 'unchecked' : !wxChecked}" />
+    </div>
+    <div class="row" @click="useZfbPay">
+      <div class="left">&nbsp;&nbsp;&nbsp;&nbsp;- 支付宝支付</div>
+      <div class="right">需支付 {{ data.needPay }} 元</div>
+      <div :calss="{'checked' : zfbChecked , 'unchecked' : !zfbChecked}" />
+    </div>
+    <div class="row" @click="useBankPay">
+      <div class="left">&nbsp;&nbsp;&nbsp;&nbsp;- 银行汇款</div>
+      <div class="right">查看说明</div>
+      <div class="icon go" />
+    </div>
+    <div class="btn" @click="doPay">确定支付</div>
+    <p class="tip">温馨提示：如果您使用微信支付过程中遇到支付限额问题，建议您使用支付宝进行支付。如有疑问，请拨打菩提果客户服务电话： 400-618-8835。</p>
   </div>
 </template>
 <style scoped>
@@ -9,6 +40,69 @@
   text-align: center;
 }
 
+.root {
+  background: '#f2f2f2';
+}
+
+.btn {
+  background: '#FF6867';
+  color: '#fff';
+  cursor: 'pointer';
+  lineHeight: 36px;
+  textAlign: 'center';
+  borderRadius: 4;
+  margin: 30px 20px;
+}
+
+.row {
+  height: 40px;
+  lineHeight: 40px;
+  position: 'relative';
+  background: '#fff';
+  borderBottom: '1px solid #ccc';
+}
+
+.header {
+  lineHeight: 3px;
+  paddingLeft: 10px;
+  borderBottom: '1px solid #ccc';
+}
+
+.left {
+  position: 'absolute';
+  left: 10px;
+}
+
+.right {
+  color: 'rgb(255, 51, 0)';
+  position: 'absolute';
+  right: 40px;
+}
+
+.icon {
+  position: 'absolute';
+  right: 10px;
+  top: 5px;
+  width: 30px;
+  height: 30px;
+  backgroundSize: 'contain';
+}
+
+.checked {
+  /*backgroundImage: 'url(' + util.i('checked.png') + ')';*/
+}
+
+.unchecked {
+  /*backgroundImage: 'url(' + util.i('unchecked.png') + ')';*/
+}
+
+.go {
+  /*backgroundImage: 'url(' + util.i('jt-right.png') + ')';*/
+}
+
+.tip {
+  margin: 30px 20px;
+}
 
 </style>
 <script>
@@ -74,10 +168,11 @@ API.doShanghu = function(orderId, cb) {
     },
     cb);
 };
+
 export default {
   mounted() {
     console.log('This is pty order pay component !');
-  
+
 
 
   },
@@ -100,12 +195,7 @@ export default {
     var pdata = JSON.parse(this.dataJsonstr);
     console.log(pdata.list);
     return {
-      list: pdata.list,
-      state: {
-        showTuijianBtn: true,
-        show_confirm: false
-
-      },
+      data:pdata
     }
   },
   computed: {
@@ -127,7 +217,18 @@ export default {
       location.reload();
 
     },
- 
+    useBalancePay() {
+      console.log('using balance');
+    },
+    useWxPay() {
+      console.log('using wx');
+    },
+    useZfbPay() {
+      console.log('using zfb');
+    },
+    useBankPay() {
+      console.log('using bank');
+    },
   }
 
 

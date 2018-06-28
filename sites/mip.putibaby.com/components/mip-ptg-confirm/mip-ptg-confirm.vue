baca06da10610108c66f5f4d4d519281745d4d37
Qi Ming <phoenixqm@gmail.com>
2018-06-28 12:18:16 +0800

    format code

--- sites/mip.putibaby.com/components/mip-ptg-confirm/mip-ptg-confirm.vue
+++ sites/mip.putibaby.com/components/mip-ptg-confirm/mip-ptg-confirm.vue
@@ -1,55 +1,51 @@
-
 <template>
-<div v-if="show" class="root">
-<div id='fullScreen'></div>
-
-<div id='floatLayer'>
-    <p>{{ msg }}</p>
-    <p><button @click="cancelConfirm">取消</button></p>
-    <p><button @click="okConfirm">确定</button></p>
-</div>
-
-
-</div>
-    
-
+  <div v-if="show" class="root">
+    <div id='fullScreen'></div>
+    <div id='floatLayer'>
+      <p>{{ msg }}</p>
+      <p>
+        <button @click="cancelConfirm">取消</button>
+      </p>
+      <p>
+        <button @click="okConfirm">确定</button>
+      </p>
+    </div>
+  </div>
 </template>
-
-
 <style scoped>
 .wrapper {
   margin: 0 auto;
   text-align: center;
 }
 
+
+
 /* https://www.cnblogs.com/martianShu/p/5893355.html */
-#fullScreen
-{
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    opacity: 0.7;
-    background-color: black;
-    z-index: 9999;
-}
-#floatLayer
-{
-    position: fixed;
-    width: 80%;
-    height: 500px;
-    left: 10%;
-    top: 15%;
-    background-color: white;
-    z-index: 10000;
+
+#fullScreen {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  opacity: 0.7;
+  background-color: black;
+  z-index: 9999;
 }
 
+#floatLayer {
+  position: fixed;
+  width: 80%;
+  height: 500px;
+  left: 10%;
+  top: 15%;
+  background-color: white;
+  z-index: 10000;
+}
 </style>
-
 <script>
-
 var API = {};
+
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
@@ -61,54 +57,53 @@ function checkStatus(response) {
 }
 
 function parseJSON(response) {
-   return response.json()
+  return response.json()
 }
 
 API.wrapRet_ = function(api, opts, cb) {
   console.log('posting to ' + api);
-  fetch(api,{  
-    method: 'POST',
-    credentials: "same-origin",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(opts)
-  })
-  .then(checkStatus)
-  .then(parseJSON)
-  .then(ret => {
-  //console.log(ret);
-    if(ret.success) cb(true, ret.data);
-    else cb(false, ret.error);
-  })  
-  .catch(e => {
-    console.error(e.message); 
-    cb(false, e.message);
-  });
+  fetch(api, {
+      method: 'POST',
+      credentials: "same-origin",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(opts)
+    })
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(ret => {
+      //console.log(ret);
+      if (ret.success) cb(true, ret.data);
+      else cb(false, ret.error);
+    })
+    .catch(e => {
+      console.error(e.message);
+      cb(false, e.message);
+    });
 }
 
 
 export default {
-  mounted () {
+  mounted() {
     console.log('This is comfirm component !');
-   var self = this;
-   this.$element.customElement.addEventAction('doshow', function (event, str)    {
-	   console.log(event);
-	   console.log(str);
-	   self.show = true;
-	   self.current_el_event = event;
-	   self.current_el_id = event.el_id;
-	   self.current_el_data = event.data;
-	   //self.$set(this, 'show', true);
-   });
+    var self = this;
+    this.$element.customElement.addEventAction('doshow', function(event, str) {
+      // console.log(event);
+      // console.log(str);
+      self.show = true;
+      self.current_el_event = event;
+      self.current_el_id = event.el_id;
+      self.current_el_data = event.data;
+    });
   },
-   firstInviewCallback () {
+  firstInviewCallback() {
     this.init()
   },
   props: {
-	show: {
-	  type: Boolean,
-	  default: false
+    show: {
+      type: Boolean,
+      default: false
     },
     msg: {
       type: String,
@@ -127,23 +122,23 @@ export default {
       default: null
     }
   },
-  data () {
+  data() {
     console.log(this);
-	return {
-		show:false
-	};
+    return {
+      show: false
+    };
 
   },
   computed: {
-    
+
   },
   watch: {
-      show: function (val, oldVal) {
-	   console.log('new: %s, old: %s', val, oldVal);
-	 }
-   },
+    show: function(val, oldVal) {
+      console.log('new: %s, old: %s', val, oldVal);
+    }
+  },
   methods: {
-    init () {
+    init() {
       console.log('should loading');
       this.reload_();
     },
@@ -156,26 +151,25 @@ export default {
 
     },
     cancelConfirm() {
-      //this.$emit(this.cancelevent);
-	  var ele = document.getElementById(this.current_el_id);
-      console.log(ele);
-	  this.current_el_event.el_id = 'ptgconfirm';
+
+      var ele = document.getElementById(this.current_el_id);
+      // console.log(ele);
+      this.current_el_event.el_id = 'ptgconfirm';
       MIP.viewer.eventAction.execute('docancel', ele, this.current_el_event);
-	  this.current_el_event = {};
+      this.current_el_event = {};
       this.current_el_id = '';
-	  this.current_el_data = {};
-	  this.show = false;
+      this.current_el_data = {};
+      this.show = false;
     },
     okConfirm() {
-      //	var ele = document.getElementById('ptgconfirm');	
-	  var ele = document.getElementById(this.current_el_id);
-      console.log(ele);
-	  this.current_el_event.el_id = 'ptgconfirm';
-	  MIP.viewer.eventAction.execute('dook', ele, this.current_el_event);
-	  this.current_el_event = {};
+      var ele = document.getElementById(this.current_el_id);
+      // console.log(ele);
+      this.current_el_event.el_id = 'ptgconfirm';
+      MIP.viewer.eventAction.execute('dook', ele, this.current_el_event);
+      this.current_el_event = {};
       this.current_el_id = '';
-	  this.current_el_data = {};
-	  this.show = false;
+      this.current_el_data = {};
+      this.show = false;
 
     }
   }

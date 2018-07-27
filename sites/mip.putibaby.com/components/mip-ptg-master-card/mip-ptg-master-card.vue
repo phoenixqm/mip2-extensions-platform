42c54fd07eac13713f3d473d80b4fae0b0c45d80
Qi Ming <qiming@putibaby.com>
2018-07-27 17:05:32 +0800

    fix card union

--- sites/mip.putibaby.com/components/mip-ptg-master-card/mip-ptg-master-card.vue
+++ sites/mip.putibaby.com/components/mip-ptg-master-card/mip-ptg-master-card.vue
@@ -1071,6 +1071,9 @@ export default {
       } else if (event.userInfo.isUnion && origin === 'order_list') {
         console.log('logindone to order_list')
         window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/order_list'), {})
+      } else if (origin === 'fav' && !event.userInfo.isUnion) {
+        console.log('go to submit_ph')
+        window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/submit_ph?to=' + encodeURIComponent(window.location.href)), {})
       } else if (origin && !event.userInfo.isUnion) {
         console.log('go to submit_ph')
         window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/' + origin), {})
@@ -1114,11 +1117,13 @@ export default {
       }
       if (!this.isUnion) {
         var to = '/' + cmd
+		console.log(to);
         if (cmd === 'fav') {
           window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/submit_ph?to=' + encodeURIComponent(window.location.href)), {})
         } else if (cmd === 'order_list') {
           window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/submit_ph?to=' + encodeURIComponent(to)), {})
         } else if (cmd === 'update_time') {
+		  to = 'https://mip.putibaby.com/update_time_mip?mcode=' + this.data.codeid
           window.MIP.viewer.open(MIP.util.makeCacheUrl('https://mip.putibaby.com/submit_ph?to=' + encodeURIComponent(to)), {})
         }
 

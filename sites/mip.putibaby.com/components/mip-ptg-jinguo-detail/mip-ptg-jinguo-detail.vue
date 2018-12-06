37f98a4515867a2879f113f5bb4c193cd63fee7e
sunzhengtao <490104472@qq.com>
2018-12-06 10:20:49 +0800

    12061020

--- sites/mip.putibaby.com/components/mip-ptg-jinguo-detail/mip-ptg-jinguo-detail.vue
+++ sites/mip.putibaby.com/components/mip-ptg-jinguo-detail/mip-ptg-jinguo-detail.vue
@@ -1,26 +1,26 @@
 
 <template>
   <div class="root">
-  <div style={ui.main}>
-            <h2 style={{fontSize:16,color:'#3f552a'}}>
+  <div style="main">
+            <h2 class="title_h2">
               <b>菩提果服务全面升级：360度+360天全程呵护</b>
             </h2>
-            <div class="line" style={{height:1,background:'#3f552a',marginBottom:15}}></div>
+            <div class="line" ></div>
 
             <div >甲方（客户）：
-              <i style={{color:'#00a0ff',borderBottom:'1px solid #999',fontStyle:'normal',paddingLeft:10,paddingRight:10}}>
-                {mama}
+              <i class="jiafang">
+                {contract_mama_name}
               </i>
             </div>
             <div >乙方（公司）：
-              <i style={{color:'#00a0ff',borderBottom:'1px solid #999',fontStyle:'normal',paddingLeft:10,paddingRight:10}}>
+              <i class="jiafang">
                 天津菩提果科技有限公司
               </i>
             </div>
 
-        <p style={{color:'#3f552a'}}><b>感谢甲方选择了菩提果……</b></p>
+        <p class="p_1" ><b>感谢甲方选择了菩提果……</b></p>
         <p>三载耕耘，十座城市，二十万月嫂，千万护理数据。围绕着大数据和互联网技术，优化母婴护理服务体验，让找月嫂/育儿师不再困难，菩提果已成为行业快速崛起的领军品牌，在妈妈圈中口碑一次又一次的得到验证，我们赢得了这样的荣誉：</p>
-        <ul style={{paddingLeft:25}} type="square">
+        <ul class="ul_25" type="square">
         <li>清科云测女性母婴中国好应用Top 50</li>
           <li>虎嗅网最值得关注的100+消费升级新品牌</li>
           <li>大众点评用户票选孕产护理人气品牌</li>
@@ -34,10 +34,10 @@
         <p>●&nbsp;<b>我们重新定义了“用母婴护理师” 。</b>基于互联网工具，月嫂/育儿师每日服务日志云端可查可追踪，同时菩提果育儿客户端提供数十项专业、方便、快捷的护理养育工具，让服务数据化，让爱有温度。同时菩提果也启用专业的保险服务和专家决策支撑系统为月嫂服务做支撑。让用月嫂/育儿师更安全，更靠谱，更贴心。</p>
 
     <p>尽管取得了一点点小小的成绩，但菩提果也深知：</p>
-    <p style={{color:'#3f552a'}}><b>一个专业的好月嫂≠一个完美无缺的好月子。</b></p>
-    <p style={{color:'#3f552a'}}><b>一个专业的好育儿师≠一个优质的育儿服务。</b></p>
+    <p class="p_1" ><b>一个专业的好月嫂≠一个完美无缺的好月子。</b></p>
+    <p class="p_1" ><b>一个专业的好育儿师≠一个优质的育儿服务。</b></p>
     <p>做为新时代的女性，您不仅仅需要一个好月嫂，不仅需要给宝宝找个好的育儿师，同时更需要：</p>
-  <ul style={{paddingLeft:25}} type="square">
+  <ul class="ul_25" type="square">
   <li>时刻在线的医护专家帮您解答护理难题</li>
   <li>个性专属的月子调理帮您快速恢复活力</li>
   <li>专业资质的产后康复帮您变得年轻美丽</li>
@@ -49,69 +49,69 @@
     <p><b>您需要的是一个服务团队，您值得更全面、更长久的呵护！</b></p>
     <p><b>菩提果不仅要帮您找到好月嫂好育儿师，更让您能体验一个完美的月子，一个优质的育儿服务体验，总之轻松一整年！</b></p>
     <p><b>所以，菩提果服务全面升级，推出360度+360天 会员服务，全程呵护让爱跟随！</b></p>
-    <p style={{color:'#3f552a'}}><b>感谢甲方成为了菩提果金果会员……</b></p>
+    <p class="p_1" ><b>感谢甲方成为了菩提果金果会员……</b></p>
     <p>在本协议约定范围中，乙方为甲方提供以下金果会员权益，下面给甲方详细说明菩提果金果会员服务包含的<b>四重大礼</b>：</p>
-    <p style={{color:'#3f552a'}}><b>一年会员尊享特权</b></p>
+    <p class="p_1" ><b>一年会员尊享特权</b></p>
     <p><b>1.&nbsp;购买月嫂或者育儿师服务立享会员优惠价</b></p>
-      <p style={{marginLeft:13}}>甲方在菩提果购买月嫂或者育儿师服务时，均可享受月嫂或者育儿师的会员价进行签单，直接获得降价的实惠和优惠。同时购买完美月子计划即签约月嫂满100天客户即可免费成为菩提果金果会员，享受特权同时立省2880元。如果是直接办理育儿会员的，可以免收签约的服务费。如果是月嫂金果会员在月子期后又在菩提果请育儿师服务的，即可享受育儿签约服务费的五折优惠。</p>
+      <p class="p_2">甲方在菩提果购买月嫂或者育儿师服务时，均可享受月嫂或者育儿师的会员价进行签单，直接获得降价的实惠和优惠。同时购买完美月子计划即签约月嫂满100天客户（月嫂私单除外）即可免费成为菩提果金果会员，享受特权同时立省2880元。如果是直接办理育儿会员的，可以免收签约的服务费。如果是月嫂金果会员在月子期后又在菩提果请育儿师服务的，即可享受育儿签约服务费的五折优惠。</p>
 
     <p><b>2.&nbsp;医护专家团队在线答疑指导</b></p>
-      <p style={{marginLeft:13}}>乙方菩提果的医生和护士组成的专家团队，将从甲方签单成为会员时起一年内（360天）全程服务，全程跟踪进行健康管理与咨询。孕产期的定期电话关怀，预产期前的电话提醒，待产包的咨询，宝宝育儿期间的护理咨询等，随时接受客户咨询与建议。定期收集客户健康信息，孕产知识在线教育。服务期间进行满意度回访。</p>
+      <p class="p_2">乙方菩提果的医生和护士组成的专家团队，将从甲方签单成为会员时起一年内（360天）全程服务，全程跟踪进行健康管理与咨询。孕产期的定期电话关怀，预产期前的电话提醒，待产包的咨询，宝宝育儿期间的护理咨询等，随时接受客户咨询与建议。定期收集客户健康信息，孕产知识在线教育。服务期间进行满意度回访。</p>
     <p><b>3.&nbsp;个性化定制护理、月子餐、辅食计划</b></p>
-      <p style={{marginLeft:13}}>由乙方专业的医护团队针对甲方的身体情况和特殊体质，进行护理的私人化定制与指导。安排营养师根据不同孕产时期给出合理化建议。在宝宝养育过程中，针对宝宝不同时期给予哺乳指导和适龄的辅食指导。</p>
+      <p class="p_2">由乙方专业的医护团队针对甲方的身体情况和特殊体质，进行护理的私人化定制与指导。安排营养师根据不同孕产时期给出合理化建议。在宝宝养育过程中，针对宝宝不同时期给予哺乳指导和适龄的辅食指导。</p>
     <p><b>4.&nbsp;菩提果育儿APP专属会员特权</b></p>
-      <p style={{marginLeft:13}}>甲方可以享有在乙方菩提果育儿APP上浏览“有问必答”、“专家在线”、“学习园地”等会员版块的权利。这些版块只针对会员服务，在这些版块里能够与专家进行在线问答，阅读专家针对母婴和孕产期最专业的知识内容，同时可以在学习园地里参与学习大量的孕产期母婴知识。</p>
+      <p class="p_2">甲方可以享有在乙方菩提果育儿APP上浏览“有问必答”、“专家在线”、“学习园地”等会员版块的权利。这些版块只针对会员服务，在这些版块里能够与专家进行在线问答，阅读专家针对母婴和孕产期最专业的知识内容，同时可以在学习园地里参与学习大量的孕产期母婴知识。</p>
     <p><b>5.&nbsp;崔玉涛老师育学园会员年卡</b></p>
-      <p style={{marginLeft:13}}>甲方可以享有崔玉涛老师创办的育学园的学习年卡。崔玉涛老师亲自在线解答疑难问题，并给予母婴问题的指导。</p>
+      <p class="p_2">甲方可以享有崔玉涛老师创办的育学园的学习年卡。学习崔玉涛老师在线讲解的各种疑难问题，帮助新手妈妈们更好的解决母婴护理中遇到的各种问题。</p>
 
-  <p style={{color:'#3f552a'}}><b>五次VIP上门服务</b></p>
-    <ol style={{paddingLeft:25}} >
+  <p class="p_1" ><b>五次VIP上门服务</b></p>
+    <ol class="ul_25" >
       <li>居室除螨或环境检测（二选一）一次</li>
       <li>专业医护人员上门育儿指导一次</li>
       <li>产后体质检测或者婴儿发育测评（二选一）一次</li>
       <li>产后骨盆修复或卵巢保养（二选一）一次</li>
       <li>中医子午流注特色护理一次</li>
     </ol>
-    <ul style={{paddingLeft:35}} type="square">
+    <ul class="ul_35" type="square">
       <li>针对以上五项上门服务，甲方将会收到一套对应的服务卡片。卡片会随着大礼包快递到甲方指定邮寄地址。</li>
       <li>所有服务卡片盖章有效，有效期见卡片背面的服务说明。</li>
       <li>以上服务均由乙方菩提果联合的第三方平台或机构提供。当甲方需要以下服务时，请提前致电菩提果客服（400-618-8835）帮甲方进行预约。</li>
       <li>当甲方签订会员协议时，乙方默认甲方认可第三方提供此项服务，乙方会将甲方的联系方式提供给第三方服务机构，根据甲方的生产或所需服务时间帮甲方安排预约服务。</li>
       <li>凡是标记“二选一”的项目，则表示在该项目中的两种服务内容，甲方可以自行根据需要选择其中一项进行。如对所选择的项目有疑问的，可以致电乙方客服电话进行咨询。</li>
     </ul>
-    <p style={{color:'#3f552a'}}><b>千元大礼包</b></p>
-      <table style={{border:'1px solid #000',width:'80%',textAlign:'center',margin:'0px auto'}}>
-        <tr >	  <th style={{background:'#e4eeda',fontSize:12}}>妈妈大礼包</th>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>定制浴巾毛巾套装</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>时尚实用妈咪包</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>菩提果收纳包</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>菩提果擦手巾</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>家用急救包</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>《菩提果母婴》杂志</td>	</tr>
+    <p class="p_1" ><b>千元大礼包</b></p>
+      <table class="table_1">
+        <tr >	  <th class="th1">妈妈大礼包</th>	</tr>
+        <tr class="tr1">	  <td>定制浴巾毛巾套装</td>	</tr>
+        <tr class="tr1">	  <td>时尚实用妈咪包</td>	</tr>
+        <tr class="tr1">	  <td>菩提果收纳包</td>	</tr>
+        <tr class="tr1">	  <td>菩提果擦手巾</td>	</tr>
+        <tr class="tr1">	  <td>家用急救包</td>	</tr>
+        <tr class="tr1">	  <td>《菩提果母婴》杂志</td>	</tr>
       </table>
-      <table style={{border:'1px solid #000',width:'80%',textAlign:'center',margin:'15px auto 0px'}}>
-        <tr >	  <th style={{background:'#e4eeda',fontSize:12}}>宝宝大礼包</th>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>宝宝祈福开光福袋</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>宝宝口水巾</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>果宝洗澡水温卡</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>宝宝便携奶粉格</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>宝宝纸尿裤/拉拉裤礼包</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>宝宝辅食粥米</td>	</tr>
-        <tr style={{border:'1px solid #000',fontSize:10.5}}>	  <td>*宝宝亲子游泳或早教（二选一）一次</td>	</tr>
+      <table class="table_2">
+        <tr >	  <th class="th1">宝宝大礼包</th>	</tr>
+        <tr class="tr1">	  <td>宝宝祈福开光福袋</td>	</tr>
+        <tr class="tr1">	  <td>宝宝口水巾</td>	</tr>
+        <tr class="tr1">	  <td>果宝洗澡水温卡</td>	</tr>
+        <tr class="tr1">	  <td>宝宝便携奶粉格</td>	</tr>
+        <tr class="tr1">	  <td>宝宝纸尿裤/拉拉裤礼包</td>	</tr>
+        <tr class="tr1">	  <td>宝宝辅食粥米</td>	</tr>
+        <tr class="tr1">	  <td>*宝宝亲子游泳或早教（二选一）一次</td>	</tr>
       </table>
-      <ul style={{paddingLeft:25}} type="square">
+      <ul class="ul_25" type="square">
         <li>大礼包中以上均为实物礼品（标记*星号的除外），如甲方是在菩提果请月嫂或育儿师服务的客户，则在月嫂或育儿师上户时为客户邮寄大礼包；如甲方是单独购买金果服务的会员，则在甲方签约成为乙方菩提果金果会员后，乙方大约7-15天内给甲方安排快递寄出。</li>
         <li>甲方只需提供详细邮寄地址，如邮寄地址发生变更，请甲方及时通知乙方。如因甲方未及时通知而导致的不必要的快递成本的增加，则乙方有权进行到付快递。由于各城市收发快递情况不同，所以收货时间长短不同，快递寄出后，乙方将提供快递单号供甲方查询收货。</li>
         <li>*宝宝亲子游泳或早教（二选一）一次： 大礼包中该服务，甲方将会收到一张对应的服务卡片。该服务均由乙方菩提果联合第三方平台提供。当甲方需要以下服务时，请提前致电乙方菩提果客服（400-618-8835）帮甲方进行预约。当甲方签订会员协议时，乙方会默认甲方认可第三方提供此项服务，乙方会将您的联系方式提供给第三方服务机构，根据甲方的生产或需求时间帮甲方安排预约服务。</li>
 
       </ul>
-    <p style={{color:'#3f552a'}}><b>早产儿守护</b></p>
-      <ol style={{paddingLeft:25}} >
+    <p class="p_1" ><b>早产儿守护</b></p>
+      <ol class="ul_25" >
         <li>可获赠价值2980元贝亲早产儿专业护理包一套</li>
         <li>聘请专业早产儿月嫂仍可享受会员价优惠</li>
         <li>早产方面医疗专家在线咨询指导服务</li>
       </ol>
-      <ul style={{paddingLeft:35}} type="square">
+      <ul class="ul_35" type="square">
         <li>早产是指妊娠不足37周的分娩现象，此时娩出的新生儿即早产儿，他们一般体重低于2kg甚至更低，身体许多器官尚未发育完全，因此在出生时需要特别护理。</li>
         <li>如甲方作为会员发生了早产状况，则由乙方赠送一套贝亲品牌的早产儿专业护理包。</li>
         <li>甲方如已经在乙方聘请一般月嫂，也可以要求重新签订为具有早产儿资质的月嫂，也可享受早产儿月嫂的会员价进行签单。之前的月嫂协议即为终止。或者在之前的月嫂服务价格上增加30%的早产儿服务费。</li>
@@ -124,7 +124,7 @@
         <p><b>360度全方位无微不至的呵护！360天全周期持续跟随的关爱！</b></p>
         <p><b>360度+360天 菩提果金果会员服务！让母婴服务更靠谱，不是一句口号，而是一句承诺！</b></p>
         <p><b>即刻开启完美月子和优质育儿里程！</b></p>
-          <ul style={{paddingLeft:15}} type="square">
+          <ul class="ul_15" type="square">
             <li>在甲方签单并购买金果会员服务时，表示甲方已了解以上《金果会员服务详情》。金果会员服务一旦购买即为生效，不予以退款，不设折现或者找零。</li>
             <li>甲方的会员服务期，从甲方在线签约之日开始起算，至360天后自动结束。服务到期后，如双方未达成书面续约，则协议自动终止。
   </li>
@@ -175,12 +175,57 @@ p {
   padding:1px 20px;
   textAlign:justify;
 }
-.jt_xx {
-  width: 8px;
-  height: 13px;
-  position: absolute;
-  right: 14px;
-  top: 16px;
+.title_h2 {
+  font-size:16px;
+  color:#3f552a;
+}
+.line {
+  height:1px;
+  background:#3f552a;
+  margin-bottom:15px;
+}
+.jiafang{
+  color:#00a0ff;
+  border-bottom:1px solid #999;
+  font-style:normal;
+  padding-left:10;
+  padding-right:10px;
+
+}
+.p_1{
+  color:#3f552a;
+}
+.p_2{
+  margin-left:13px;
+}
+.ul_15{
+  padding-left:15px;
+}
+.ul_25{
+  padding-left:25px;
+}
+.ul_35{
+  padding-left:35px;
+}
+.table_1 {
+border:1px solid #000;
+width:80%;
+text-align:center;
+margin:0px auto;
+}
+.table_2 {
+border:1px solid #000;
+width:80%;
+text-align:center;
+margin:15px auto 0px;
+}
+.tr1 {
+  border:1px solid #000;
+  font-size:10.5px;
+}
+.th1 {
+  background:#e4eeda;
+  font-size:12px;
 }
 
 </style>
